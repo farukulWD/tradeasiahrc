0b03cccfc1c22b511498ba9e41b8dfb4b14a922c
farukulWD <farukgb1999@gmail.com>
2024-03-19 05:25:41 +0600

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/common/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "TRADE ASIA HRC (PVT.)",
+  title: "TRADE ASIA HRC (PVT.) LIMITED",
   description:
     "THE TRADE ASIA HRC (PVT.) LIMITED HAS STARTED OPERATION AS AN ENGINE OIL IMPORTER COMPANY.Also we involved with business like Export, Import, Supply, Manufacturing, Real estate, Construction, Agro, Information Technology, Chemical, Pharmaceutical, E-commerce, Food and Beverage.We strive relentlessly to meet and exceed applicable standards and bear a great responsibility for people, machines and the environment. Satisfied and delighted customers are the top priority in our corporate philosophy. We offer great back up support services to our customers so as together we tackle the difficult task and solve them with both experience and commitments in focused team work.",
 };
