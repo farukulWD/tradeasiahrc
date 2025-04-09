14bed6af549fff7167af9754b09ca7c11d58da41
farukulWD <farukgb1999@gmail.com>
2025-04-09 23:15:16 +0600

    add pipline

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function products() {
   return (
     <div>
+
       
       <div className="h-[calc(100vh-64px)] flex justify-center items-center">
         <h1 className="text-4xl">Coming soon</h1>
