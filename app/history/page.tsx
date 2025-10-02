877ac7bf1302a02cdd1ed9eb7afd3c2170053ae3
farukulWD <farukgb1999@gmail.com>
2025-10-03 01:19:12 +0600

    new setup with vps

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <div>
       <div className="h-[calc(100vh-64px)] flex justify-center items-center">
-        <h1 className="text-4xl">Coming soon</h1>
+        <h1 className="text-4xl">Coming soon our history</h1>
       </div>
     </div>
   );
